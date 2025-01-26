f7e1afca410d80c457b34670d47a43cf4556ebd6
Petr Grabovský <petr@grabovsky.tech>
2025-01-26 16:58:00 +0100

    fix: Oprav problém vyhledávání 'JSON Serveru'
    - Downgraduj verzi 'JSON Serveru' kvůli nefunčnímu vyhledávání pomocí 'q' parametru v nové verzi
    - Přidej řádek do modulu 'getUrl.ts' pro zajištění správného řazení výsledků ve starší verzi 'JSON Serveru'

--- src/app/utils/getUrl.ts
+++ src/app/utils/getUrl.ts
@@ -2,7 +2,8 @@ const API_URL = process.env.NEXT_PUBLIC_API_URL!;
 
 export const getUrl = () => {
   const url = new URL(API_URL);
-  url.searchParams.append('_sort', '-publishedAt');
+  url.searchParams.append('_sort', 'publishedAt');
+  url.searchParams.append('_order', 'desc');
 
   return url;
 };
